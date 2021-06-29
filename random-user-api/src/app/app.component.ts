5869572c39d66894fc9b540c244a7426995a1c0a
ephraim9 <ephraimg@discovery.co.za>
2021-06-29 16:34:33 +0200

    Connect to API

--- random-user-api/src/app/app.component.ts
+++ random-user-api/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'random-user-api';
+
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+
+  private fetchPosts() {
+    this.http
+      .get('https://randomuser.me/api/')
+      .subscribe(posts => {
+        // ...
+        console.log(posts);
+      });
+  }
 }
